b2f2a3d2cda3769be60c0c8843fe2632b9a47e67
Alexandre Delille <adelille@student.42.fr>
2021-03-02 14:33:03 +0100

    add link about info on ASM

--- libasm.h
+++ libasm.h
@@ -6,13 +6,16 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 14:05:16 by adelille          #+#    #+#             */
-/*   Updated: 2021/03/02 14:06:30 by adelille         ###   ########.fr       */
+/*   Updated: 2021/03/02 14:32:38 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBASM_H
 # define LIBASM_H
 
+	/* Help to understand ASM: https://en.wikibooks.org/wiki/X86_Assembly */
 # include <stdio.h>
 
 size_t	ft_strlen(char *str);
+
+#endif
