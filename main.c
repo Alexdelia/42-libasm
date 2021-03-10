770ce812e041ac4f9141d85d501fdebfda72bb43
Alexdelia <alexandre.delille.57@gmail.com>
2021-03-10 16:42:22 +0100

    changing main.c

--- main.c
+++ main.c
@@ -6,35 +6,39 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 13:51:44 by adelille          #+#    #+#             */
-/*   Updated: 2021/03/02 14:15:54 by adelille         ###   ########.fr       */
+/*   Updated: 2021/03/10 16:41:48 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		main(int ac, char **av)
+#define MAG		"\033[35m"
+#define BMAG	"\033[0;35m"
+#define DEF		"\033[0m"
+
+int		ft_ps(char *str)
 {
-	int		i;
-	char	s1[256];
-	char	s2[0];
-	char	s3[3] = "oui";
-	char	s4[10] = "0123456789";
+	int	i;
 
-	if (ac > 1)
-		s1 = av[1];
-	 else
-	 {
-		 i = 0;
-		 while (s1[i])
-		 {
-			s1[i] = (char)(i + 0);
-		 }
-	 }
-	 printf("\tStart:");
+	i = 0;
+	while (str[i])
+		i++;
+	write(1, str, i);
+	return (i);
+}
 
-	 printf("ft_strlen:\n");
-	 printf("Me\t%d\tLibc%d\tInput:%.10s (cut at 10 char)\n", ft_strlen(s1), strlen(s1), s1);
-	 printf("Me\t%d\tLibc%d\tInput:%s\n", ft_strlen(s2), strlen(s2), s2);
-	 printf("Me\t%d\tLibc%d\tInput:%s\n", ft_strlen(s3), strlen(s3), s3);
-	 printf("Me\t%d\tLibc%d\tInput:%s\n", ft_strlen(s4), strlen(s4), s4);
-	 
-	 return (0);
+void	t_strlen(void)
+{
+	
+}
+
+int		main(void)
+{
+	ft_psc("BMAG\n\t---\tStarting\t---\t\nDEF");
+	/*t_strlen();
+	t_strcmp();
+	t_strcpy();
+	t_write();
+	t_read();
+	t_strdup();*/
+	ft_psc("\n\t---\t  Done  \t---\t\n");
+	return (0);
 }
