b72ba151a6ffc865f450005edcdb4c64e2fed873
Alexdelia <alexandre.delille.57@gmail.com>
2021-03-14 18:59:10 +0100

    added tester of write

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 13:51:44 by adelille          #+#    #+#             */
-/*   Updated: 2021/03/10 18:42:11 by adelille         ###   ########.fr       */
+/*   Updated: 2021/03/14 18:48:44 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,14 +82,36 @@ void	t_strcpy(void)
 	printf("\ninput:\t\"%s\"\t(dest[10])\n your:\t%s\n  ref:\t%s\t%s\n\n", str, ft_strcpy(s_dest, str), strcpy(sr_dest, str), (ft_strcmp(y_dest, r_dest) == 0 ? "\033[1;32m[OK]\033[0m" : "\033[1;5;31m[KO]\033[0m\t\033[3;33mThis case can be negliged\033[0m"));*/
 }
 
+void	t_write(void)
+{
+	char	*s;
+
+	ft_psc("  ~~ ft_write ~~\n", IMAG);
+	s = STR;
+	ft_psc(" output of write:\n", "\033[5;3;34m");
+	printf("\n\ninput:\t\"%s\"\n your:\t%zu\n  ref:\t%zd\t%s\n\n", s, ft_write(1, s, ft_strlen(s)), write(1, s, ft_strlen(s)), ((size_t)ft_write(1, s, ft_strlen(s)) == (size_t)write(1, s, ft_strlen(s)) ? "\033[1;32m[OK]\033[0m" : "\033[1;5;31m[KO]\033[0m"));
+	s = STR2;
+	ft_psc(" output of write:\n", "\033[5;3;34m");
+	printf("\n\ninput:\t\"%s\"\t(fd = 4)\n your:\t%zu\n  ref:\t%zd\t%s\n\n", s, ft_write(4, s, ft_strlen(s)), write(4, s, ft_strlen(s)), ((size_t)ft_write(4, s, ft_strlen(s)) == (size_t)write(4, s, ft_strlen(s)) ? "\033[1;32m[OK]\033[0m\t\033[3;34mResult is due to casting as (size_t), work has intended\033[0m" : "\033[1;5;31m[KO]\033[0m"));
+	s = "";
+	ft_psc(" output of write:\n", "\033[5;3;34m");
+	printf("\n\ninput:\t\"%s\"\n your:\t%zu\n  ref:\t%zd\t%s\n\n", s, ft_write(1, s, ft_strlen(s)), write(1, s, ft_strlen(s)), ((size_t)ft_write(1, s, ft_strlen(s)) == (size_t)write(1, s, ft_strlen(s)) ? "\033[1;32m[OK]\033[0m" : "\033[1;5;31m[KO]\033[0m"));
+	s = STR;
+	ft_psc(" output of write:\n", "\033[5;3;34m");
+	printf("\n\ninput:\t\"%s\"\t\t(underwriting)\n your:\t%zu\n  ref:\t%zd\t%s\n\n", s, ft_write(1, s, 10), write(1, s, 10), ((size_t)ft_write(1, s, 10) == (size_t)write(1, s, 10) ? "\033[1;32m[OK]\033[0m" : "\033[1;5;31m[KO]\033[0m"));
+	s = STR2;
+	ft_psc(" output of write:\n", "\033[5;3;34m");
+	printf("\n\ninput:\t\"%s\"\t(overwriting)\n your:\t%zu\n  ref:\t%zd\t%s\n\n", s, ft_write(1, s, 35), write(1, s, 35), ((size_t)ft_write(1, s, 35) == (size_t)write(1, s, 35) ? "\033[1;32m[OK]\033[0m" : "\033[1;5;31m[KO]\033[0m"));
+}
+
 int		main(void)
 {
 	ft_psc("\n\t---\t Starting\t---\t\n", BIMAG);
 	t_strlen();
 	t_strcmp();
 	t_strcpy();
-	/*t_write();
-	t_read();
+	t_write();
+	/*t_read();
 	t_strdup();*/
 	// ft_psc("\n\t---\t  Bonus \t---\t\n", BIMAG);
 	ft_psc("\n\t---\t   Done  \t---\t\n\n", BIMAG);
